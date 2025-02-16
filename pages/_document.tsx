0901bda2a36bf996986131550d2ca0e18d5908a9
shielamae02 <shiela.mlepon@gmail.com>
2025-02-17 02:35:17 +0800

    chore: remove meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="viewport" content="width=content-width initial-scale=1" />
-        <meta name="description" content="A simple movie searching app." />
-        <meta charSet="UTF-8" />
-      </Head>
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
