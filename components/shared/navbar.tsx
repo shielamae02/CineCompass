2edcd3388de972ce9dbaec6fa42bcb1f543ab741
shielamae02 <shiela.mlepon@gmail.com>
2025-02-17 02:34:46 +0800

    feat: update Navbar title from 'CineFinder' to 'CineCompass'

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -3,7 +3,7 @@ import { Moon } from "lucide-react";
 const Navbar = () => {
   return (
     <nav className="py-6 flex justify-between" aria-label="Cine Finder">
-      <h1 className="text-2xl font-semibold">CineFinder</h1>
+      <h1 className="text-2xl font-semibold">CineCompass</h1>
       <button className="hover:bg-zinc-900/100 hover:border-zinc-700/50 size-10 place-items-center rounded-lg">
         <Moon className="size-5" />
       </button>
