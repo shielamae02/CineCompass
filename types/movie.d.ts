1a168cf8264ba4cbc69ea5f8045676150c43dee8
shielamae02 <shiela.mlepon@gmail.com>
2025-02-17 17:01:00 +0800

    refactor: simplify Movie type definition by removing unused properties

--- types/movie.d.ts
+++ types/movie.d.ts
@@ -1,34 +1,3 @@
-// export type Movie = {
-//   Title: string;
-//   Year: string;
-//   Rated: string;
-//   Released: string;
-//   Runtime: string;
-//   Genre: string;
-//   Director: string;
-//   Writer: string;
-//   Actors: string;
-//   Plot: string;
-//   Language: string;
-//   Country: string;
-//   Awards: string;
-//   Poster: string;
-//   Ratings: Rating[];
-//   Metascore: string;
-//   imbRating: string;
-//   imbID: string;
-//   Type: string;
-//   DVD: string;
-//   BoxOffice: string;
-//   Production: string;
-//   Website: string;
-// };
-
-// export type Rating = {
-//   source: string;
-//   value: string;
-// };
-
 export type Movie = {
   id: number;
   adult: boolean;
